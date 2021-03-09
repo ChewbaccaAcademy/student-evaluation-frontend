24da5901bd83892f21868256f83031c7600b91aa
DonSabotini <71654802+DonSabotini@users.noreply.github.com>
2021-03-09 23:47:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static('./dist/angular-app-heroku'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/team-three-frontend/'}),
+    res.sendFile('index.html', {root: 'dist/team-three-frontend/src'}),
 );
 
 // Start the app by listening on the default Heroku port
