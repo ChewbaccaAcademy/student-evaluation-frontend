06fc14f1fb3d8f386c71210319c5adb1269a517d
DonSabotini <kemesis123@gmail.com>
2021-03-10 00:48:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const app = express();
+app.use(requireHTTPS);
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/team-three-frontend'));
+app.use(express.static(’./dist/student-evaluation-frontend’));
 
-// Start the app by listening on the default Heroku port
+app.get(’/*’, function(req, res) {
+  res.sendFile(’index.html’, {root: 'dist/student-evaluation-frontend/’}
+);
+});
+
 app.listen(process.env.PORT || 8080);
-
-app.get('/*', function(req,res) {
