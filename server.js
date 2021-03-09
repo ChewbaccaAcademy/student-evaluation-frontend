caf40f32cd822e2a7f80d370a2dcb6661503cebd
DonSabotini <71654802+DonSabotini@users.noreply.github.com>
2021-03-10 00:41:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/team-three-frontend'));
+app.use(express.static(__dirname + './dist/team-three-frontend'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/team-three-frontend/src/index.html'));
-});
-console.log(path);
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req,res) {
