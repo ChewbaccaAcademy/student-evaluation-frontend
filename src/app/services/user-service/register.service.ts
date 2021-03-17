710ad0a1c785da2d0ada25c411e6a8bc8d74bd59
Evaldas Grublys <evaldas.grublys@gmail.com>
2021-03-17 11:35:10 +0200

    Created registration service and implemented in register form.

---
+++ src/app/services/user-service/register.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, mapTo } from 'rxjs/operators';
+import { User } from './model/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RegisterService {
+  constructor(private httpClient: HttpClient) {}
+
+  registerUser(user: User): Observable<any> {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
+    return this.httpClient
+      .post<User>('https://team-three-backend.herokuapp.com/signup', {
+        username: user.username,
+        password: user.password,
+        stream: user.stream.toUpperCase(),
+        email: user.email,
+      }, { headers, responseType: 'text' as 'json' }).pipe(mapTo(true), catchError(error => {
+        return of(JSON.parse(error.error).message);
+      }));
+  }
+}
