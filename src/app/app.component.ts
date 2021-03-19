31e339e9b0f4c32b47b5d3fef70b0fb204698f7f
Nikolaj Grigorjev <grigorjev.nikolaj@gmail.com>
2021-03-19 14:21:58 +0200

    navbar component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from './shared/menu-item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public menuItems: MenuItem[];
+
+  ngOnInit(): void {
+    this.menuItems = [
+      {
+        path: '/link1',
+        title: 'Link 1',
+      },
+      {
+        path: '/link1',
+        title: 'Link 2',
+      },
+      {
+        path: '/link1',
+        title: 'Link 3',
+      },
+      {
+        path: '/link1',
+        title: 'Link 4',
+      },
+      {
+        path: '/link1',
+        title: 'Link 5',
+      },
+    ];
+  }
+
   title = 'student-evaluation-frontend';
 }
