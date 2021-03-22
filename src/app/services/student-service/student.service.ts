ee57c7864d7d9060f4a2d5d5fb5dfc6def1033d5
Evaldas Grublys <evaldas.grublys@gmail.com>
2021-03-22 13:10:26 +0200

    Implemented student modify service.

--- src/app/services/student-service/student.service.ts
+++ src/app/services/student-service/student.service.ts
@@ -1,3 +1,4 @@
+import { Student } from './../user-service/model/student';
 import { UserApiInterceptorService } from './../interceptors/user-api-interceptor.service';
 import { HttpClient, HttpBackend, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -5,32 +6,30 @@ import { Injectable } from '@angular/core';
 
 const URL = 'https://team-three-backend.herokuapp.com';
 
-interface Student {
-  id: number;
-  name: string;
-  lastname: string;
-  university: string;
-  comment: string;
-  imageUrl?: string;
-}
-
 @Injectable({
   providedIn: 'root',
 })
 export class StudentService {
   private httpClient: HttpClient;
+  private reqHeader: HttpHeaders = new HttpHeaders({ 'Authorization': 'Bearer '+this.userApiInterceptorService.getSessionToken() });
 
   constructor(private httpBackend: HttpBackend, private userApiInterceptorService: UserApiInterceptorService) {
     this.httpClient = new HttpClient(httpBackend);
   }
 
-  addStudent(student: FormData): Observable<Student> {
-    const reqHeader = new HttpHeaders({ 'Authorization': 'Bearer '+this.userApiInterceptorService.getSessionToken() });
-    return this.httpClient.post<Student>(`${URL}/student`, student, { headers: reqHeader });
+  getAllStudents(): Observable<Student[]> {
+    return this.httpClient.get<Student[]>(`${URL}/student`, { headers: this.reqHeader });
   }
 
-  getAllStudents(): Observable<Student[]> {
-    const reqHeader = new HttpHeaders({ 'Authorization': 'Bearer '+this.userApiInterceptorService.getSessionToken() });
-    return this.httpClient.get<Student[]>(`${URL}/student`, { headers: reqHeader });
+  getStudentById(studentId: number): Observable<Student> {
+    return this.httpClient.get<Student>(`${URL}/student/${studentId}`, { headers: this.reqHeader });
+  }
+
+  addStudent(student: FormData): Observable<Student> {
+    return this.httpClient.post<Student>(`${URL}/student`, student, { headers: this.reqHeader });
+  }
+
+  updateStudent(student: Student): Observable<Student> {
+    return this.httpClient.put<Student>(`${URL}/student/${student.id}`, student, { headers: this.reqHeader });
   }
 }
