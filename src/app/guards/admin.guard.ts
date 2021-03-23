b1455710b3c385b59910079b619b87815cd7ab04
Nikolaj Grigorjev <grigorjev.nikolaj@gmail.com>
2021-03-23 11:12:50 +0200

    Admin guard implemented

---
+++ src/app/guards/admin.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { UserApiInterceptorService } from 'student-evaluation-frontend/src/app/services/interceptors/user-api-interceptor.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+
+  constructor(private router: Router, private userApiInterceptorService: UserApiInterceptorService, private toastr: ToastrService,) {}
+
+  canActivate() {
+    if (this.userApiInterceptorService.getSessionToken() != null && this.userApiInterceptorService.getSessionUserRole() != "ADMIN"  ) {
+console.log(this.userApiInterceptorService.getSessionToken() == null && this.userApiInterceptorService.getSessionUserRole() != "ADMIN");
+
+      this.toastr.error('Only Admin can add students', 'Error', { positionClass: 'toast-bottom-center' });
+      console.log("Pize2")
+      return false;
+    }else if(this.userApiInterceptorService.getSessionToken() == null){
+      this.router.navigate(['login']);
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
+}
