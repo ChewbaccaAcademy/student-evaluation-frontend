51925f0b76d42c50cda7f4d0fac3e129983f1d3d
Evaldas Grublys <evaldas.grublys@gmail.com>
2021-03-30 00:38:26 +0300

    Modified student list and myevaluations component style.

--- src/app/user-evaluations/user-evaluations.component.ts
+++ src/app/user-evaluations/user-evaluations.component.ts
@@ -15,6 +15,7 @@ import {
 } from '../shared/evaluation-form-globals';
 import { Router } from '@angular/router';
 import { faTrashAlt, faEdit } from '@fortawesome/free-regular-svg-icons';
+import { faAddressCard } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-user-evaluations',
@@ -41,6 +42,7 @@ export class UserEvaluationsComponent implements OnInit {
   ];
   public faTrashAlt = faTrashAlt;
   public faEdit = faEdit;
+  public faAddressCard = faAddressCard;
 
   constructor(
     private evaluationService: EvaluationService,
@@ -52,6 +54,7 @@ export class UserEvaluationsComponent implements OnInit {
   ngOnInit(): void {
     this.evaluationService.getAllUserStudentEvaluations().subscribe((value) => {
       this.userEvaluations = value;
+      console.log(this.userEvaluations);
     });
   }
 
